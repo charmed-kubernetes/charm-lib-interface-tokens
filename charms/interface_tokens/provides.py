5772c8d9c7b9355ba9d8fb30cf6c4244be2956be
Mateo Florido <mateo.florido@canonical.com>
2023-10-13 12:10:28 -0500

    Initial implementation

---
+++ charms/interface_tokens/provides.py
@@ -0,0 +1,67 @@
+import json
+from dataclasses import dataclass
+from typing import List, Optional
+
+from ops import CharmBase, Relation, Unit
+
+
+@dataclass
+class TokenRequest:
+    """Represents a request for an authentication token.
+
+    Attributes:
+        unit (str): The name of the unit making the request.
+        user (str): The user associated with the token request.
+        group (str): The group associated with the user.
+    """
+
+    unit: str
+    user: str
+    group: str
+
+
+class TokensProvider:
+    """Implements the Provides side of the tokens interface."""
+
+    def __init__(self, charm: CharmBase, endpoint: str):
+        self.charm = charm
+        self.endpoint = endpoint
+
+    def send_token(self, request: TokenRequest, token: str) -> None:
+        """Send the token to the related units."""
+        # Aggregate tokens from all relations.
+        tokens = {
+            k: v
+            for relation in self.relations
+            for k, v in json.loads(relation.data[self.unit].get("tokens", "{}"))
+        }
+
+        tokens[request.user] = token
+        value = json.dumps(tokens)
+
+        # Update all relations with the new token data.
+        for relation in self.relations:
+            relation.data[self.unit]["tokens"] = value
+
+    @property
+    def token_requests(self) -> List[TokenRequest]:
+        """List of token requests based on relations."""
+        if not self.relations:
+            return []
+        return [
+            TokenRequest(unit=unit.name, user=user, group=group)
+            for relation in self.relations
+            for unit in relation.units
+            if (user := relation.data[unit].get("user"))
+            and (group := relation.data[unit].get("group"))
+        ]
+
+    @property
+    def relations(self) -> Optional[List[Relation]]:
+        """List of relations of this endpoint."""
+        return self.charm.model.relations.get(self.endpoint)
+
+    @property
+    def unit(self) -> Unit:
+        """Return the charm unit."""
+        return self.charm.unit
---
+++ charms/interface_tokens/requires.py
@@ -0,0 +1,75 @@
+import logging
+from typing import Dict, Optional
+
+from ops import CharmBase, Relation, RelationBrokenEvent
+from ops.framework import Object
+from pydantic import BaseModel, Json, ValidationError
+
+log = logging.getLogger("ClusterAuthTokenRequirer")
+
+
+class Data(BaseModel):
+    """Represents the data structure for tokens."""
+
+    tokens: Json[Dict[str, str]]
+
+
+class TokensRequirer(Object):
+    """Handles the requirer side of the tokens relation."""
+
+    def __init__(self, charm: CharmBase, endpoint: str = "tokens"):
+        super().__init__(charm, f"relation-{endpoint}")
+        self.endpoint = endpoint
+
+    def request_token(self, user: str, group: str):
+        """Request a token for the given user and group."""
+        if self.relation:
+            self.relation.data[self.model.unit].update({"user": user, "group": group})
+
+    @property
+    def relation(self) -> Optional[Relation]:
+        """Retrieve the realtion for the given endpoint."""
+        return self.model.get_relation(self.endpoint)
+
+    @property
+    def _data(self) -> Optional[Data]:
+        """Aggregate data from all units in the relation."""
+        if not self.relation or not self.relation.units:
+            return None
+
+        data = {
+            k: v
+            for unit in self.relation.units
+            for k, v in self.relation.data[unit].items()
+        }
+        try:
+            return Data(**data)
+        except ValidationError:
+            return None
+
+    @property
+    def is_ready(self):
+        """Check if the relation data is ready and valid."""
+        data = self._data
+        if data is None:
+            log.exception(f"{self.endpoint} relation data not yet available.")
+            return False
+        return True
+
+    def evaluate_relation(self, event) -> Optional[str]:
+        """Evaluate the state of the relation."""
+        no_relation = not self.relation or (
+            isinstance(event, RelationBrokenEvent) and event.relation is self.relation
+        )
+        if not self.is_ready:
+            if no_relation:
+                return f"Missing required {self.endpoint} relation"
+            return f"Waiting for {self.endpoint} relation"
+        return None
+
+    def get_token(self, user) -> Optional[str]:
+        """Return the token for the given user."""
+        if not self.is_ready:
+            return None
+
+        return self._data.tokens.get(user)
